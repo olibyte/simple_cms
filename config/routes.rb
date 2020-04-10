b0e6dfcd33805914d73290dc2a251936b662dca8
ocben1 <ocben1@gmail.com>
2020-04-10 17:36:49 +1000

    first routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
   get 'demo/index'
+
+  #default route
+  # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
