07bb2f6dae97dcb6f7600ab2849cebf1bbf0dd97
ocben1 <ocben1@gmail.com>
2020-04-11 13:55:14 +1000

    using strong parameters to regulate input

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -12,6 +12,16 @@ class SubjectsController < ApplicationController
   end
 
   def create
+    #Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    #save the object
+    if @subject.save
+      #If save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      #If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
@@ -26,4 +36,10 @@ class SubjectsController < ApplicationController
   def destroy
   end
   
+  private
+
+    def subject_params
+      params.require(:subject).permit(:name, :visible, :position)
+    end
+
 end
