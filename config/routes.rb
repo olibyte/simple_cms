4d8eea2ff521e810a64d93df2efac326f121ad75
ocben1 <ocben1@gmail.com>
2020-04-11 12:53:48 +1000

    add resourceful

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,37 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
 
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
-  
   root 'demo#index'
 
   get 'demo/index'
   get 'demo/hello'
   get 'demo/about'
   get 'demo/contact'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
 
   #default route
   # get ':controller(/:action(/:id))'
